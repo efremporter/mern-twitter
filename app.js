33a21fc01e307394b34aa676b216259daa9df87c
Efrem <eopiscool@gmail.com>
2022-02-06 18:09:40 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+app.get("/", (req, res) => res.send("Hello World"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
